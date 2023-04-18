73a56f01106231da0028d1eac6e711fea281f1c8
himanshusagar <himanshusagar@protonmail.com>
2023-04-18 11:00:57 -0500

    Works on Server

--- hs_app/app.py
+++ hs_app/app.py
@@ -1,10 +1,30 @@
+import time
+
+import redis
 from flask import Flask
+
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+def hello():
+    count = get_hit_count()
 
+    with open("/data/output.txt", "a") as text_file:
+        text_file.write( str(count)  + "\n" )
 
-if __name__ == "__main__":
-    app.run(debug=False, port=9989)
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+   
+
